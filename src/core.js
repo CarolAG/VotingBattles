eaf3d5b721ddd0baf5ff0312cdc5f051cf0c060d
carol Gonzalez <carol.argentina.gonzalez@gmail.com>
2016-04-18 19:21:14 -0700

    created a function "next" that creates the next state tree as a voting poll begins. The function creates a new state object/tree and the new "next" state object/tree now has one more key value pair "vote" that is two items from the entry list that are being voted upon.The new state tree is being created with a .merge method from the immutable lib. There is also a test that is ensuring that this second state tree is an obj with an entries key that has two less items in the list and new "vote" key holding an object with a "pair" key holding the two items up for vote

--- src/core.js
+++ src/core.js
@@ -1,6 +1,18 @@
-import {List} from 'immutable'
+import {List, Map} from 'immutable'
 
 export function setEntries (state, entries) {
   console.log('using .set', state.set('entries', entries))
   return state.set('entries', List(entries))
+}
+
+export function next (state) {
+  const entries = state.get('entries')
+  console.log('is this the new state, the next?', state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  }))
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  })
 }
