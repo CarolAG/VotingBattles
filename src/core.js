0a3275e782faebc46112651f4656e10520d6094e
carol Gonzalez <carol.argentina.gonzalez@gmail.com>
2016-04-19 15:31:04 -0700

    created function to check who the winner of a vote is, it checks the tallies and compares them and returns the winner, "getWinner" func. Then modified the "next" func to concat the winner, that "getWinner" returns, to the entries list

--- src/core.js
+++ src/core.js
@@ -5,8 +5,19 @@ export function setEntries (state, entries) {
   return state.set('entries', List(entries))
 }
 
+function getWinners(vote){
+  if (!vote) return [];
+  const [a,b] = vote.get('pair')
+  const aVotes = vote.getIn(['tally', a], 0)
+  const bVotes = vote.getIn(['tally', b], 0)
+  if      (aVotes > bVotes) return [a]
+  else if (aVotes < bVotes) return [b]
+  else                    return [a,b]
+}
+
 export function next (state) {
   const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')))
   console.log('is this the new state, the next?', state.merge({
     vote: Map({pair: entries.take(2)}),
     entries: entries.skip(2)
