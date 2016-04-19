7e1ef56cbf5f1f312a3a68f189f7c304dfb2a548
carol Gonzalez <carol.argentina.gonzalez@gmail.com>
2016-04-18 20:23:13 -0700

    function that counts votes for entries and test to make sure counting of votes is happening

--- test/core_spec.js
+++ test/core_spec.js
@@ -21,6 +21,7 @@ describe ('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }))
     })
+
   })
   describe('next', () => {
     it ('takes the next two entries under vote', () => {
@@ -35,8 +36,54 @@ describe ('application logic', () => {
         entries: List.of('Sunshine')
       }))
     })
+
   })
-  
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+        }),
+        entries: List()
+      })
+      const nextState = vote(state, 'Trainspotting')
+      expect(nextState).to.equal{Map({
+        vote: Map({
+          pair:List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting':1
+          })
+        }),
+        entries: List()
+      })
+      }
+    })
+    it('adds to existing tally forthe voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally:Map({
+            'Trainspotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      })
+      const nextState = vote(state, 'Trainspotting')
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      }))
+    })
+
+  })
+
 })
 
 // Our initial implementation of setEntries can just do the simplest thing
